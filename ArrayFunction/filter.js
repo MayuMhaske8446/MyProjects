d28c9c9a810b3d48ec5ab03c3e3b9cdb0943f01c
Mayur Mhaske <mayumhaske8446@outlook.com>
2022-09-12 17:33:12 +0530

    "added another example"

--- ArrayFunction/filter.js
+++ ArrayFunction/filter.js
@@ -1,3 +1,4 @@
+// Example 1
 const ages = [13, 19, 25, 14, 36, 27, 40];
 
 function isAdult(age){
@@ -5,6 +6,8 @@ function isAdult(age){
 }
 
 console.log(ages.filter(isAdult));
+
+// Example 2
 
 let cities = [
     {name: "Los Angeles", population: 3792621},
@@ -23,3 +26,23 @@ for(let i = 0; i < cities.length; i++){
 }
 
 console.log(bigCities);
+
+// Example 3: using thisArg value passed
+
+function isInRange(value){
+    if( typeof value !== "number"){
+        return false;
+    }
+    return value >= this.lower && value <= this.upper;
+}
+
+range = {
+    lower: 1,
+    upper: 10
+}
+
+let data = [10, 20, "30", 1, 5, "Javascript filter", undefined, "example"];
+
+let numberInRange = data.filter(isInRange, range);
+
+console.log(numberInRange);
